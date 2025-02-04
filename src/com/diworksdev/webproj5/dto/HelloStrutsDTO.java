26122bb85323deadad4c90f144ad15dfab8f308e
yunahamazawa <yunahamazawa821@gmail.com>
2025-02-04 20:29:59 +0900

    HelloStrutsDTO作成

---
+++ src/com/diworksdev/webproj5/dto/HelloStrutsDTO.java
@@ -0,0 +1,16 @@
+package com.diworksdev.webproj5.dto;
+
+public class HelloStrutsDTO {
+	private String result;
+
+
+	public String getResult() {
+		return result;
+	}
+
+
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+}
